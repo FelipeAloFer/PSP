11f2641cc2c7e87e65ffe4a40a2f94a277e84ec2
FelipeAloFer <felipe.alonsofernandez@iesvalleinclan.es>
2024-09-26 14:41:50 +0200

    26 de Sept

--- src/Tema1/LeerNombre.java
+++ src/Tema1/LeerNombre.java
@@ -1,17 +1,18 @@
 package Tema1;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 public class LeerNombre {
-
     public static void main(String[] args) throws IOException {
-        System.out.println(args.toString());
-        if (args.length > 0) {
-            System.out.println(args);
+        if (args.length <= 0) {
+            System.err.println("Se necesita un argumento");
             System.exit(1);
         } else {
-            System.err.println("Se necesita un programa a ejecutar");
-            System.exit(-1);
+            System.out.println(Arrays.toString(args));
+            System.exit(0);
         }
     }
 }
+
