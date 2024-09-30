6b7a583950f4157203383dec3d4d6b02093c45e5
FelipeAloFer <felipe.alonsofernandez@iesvalleinclan.es>
2024-09-30 08:47:34 +0200

    26 de Sept

---
+++ src/Tema1/EjecucionLeerNombre.java
@@ -0,0 +1,23 @@
+package Tema1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class EjecucionLeerNombre {
+    public static void main(String[] args) throws IOException {
+        Process p = new ProcessBuilder("java", "/home/usuario/Escritorio/PSP/src/Tema1/LeerNombre.java", "ArgumentoImaginario").start();
+
+        int exitVal;
+        try {
+            exitVal = p.waitFor();
+            System.out.println("Valor de salida: " + exitVal);
+//            if (exitVal == 0) {
+//                System.out.println(Arrays.toString());
+//            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
