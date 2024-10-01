e47683b5d3f3dcab5ea81006172baae115cc0464
FelipeAloFer <felipe.alonsofernandez@iesvalleinclan.es>
2024-10-01 14:34:11 +0200

    26 de Sept

---
+++ src/pack2/Ejercicio1_4.java
@@ -0,0 +1,30 @@
+package pack2;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Ejercicio1_4 {
+    public static void main(String[] args) throws IOException {
+        File directorio = new File("/bin");
+
+        ProcessBuilder pb = new ProcessBuilder("/home/usuario/.jdks/openjdk-23-1/bin/java", "Ejemplo2");
+
+        pb.directory(directorio);
+
+        System.out.printf("Directorio de trabajo: %s%n",pb.directory());
+
+        Process p = pb.start();
+
+        try {
+            InputStream is = p.getInputStream();
+            int c;
+            while ((c = is.read()) != -1) {
+                System.out.print((char) c);
+            is.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
